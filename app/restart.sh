551c5253713b2fc05abcf38055c402dde74a5224
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-10-23 13:46:53 +0800

    add restart shell

--- app/restart.sh
+++ app/restart.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-lsof -i:11001
+port=11001
+# 根据端口号查询对应的pid
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+
+# 杀掉对应的进程，如果pid不存在，则不执行
+if [  -n  "$pid"  ];  then
+    kill  -9  $pid;
+fi
+
+go build
+nohup ./app 2>&1 &
---
+++ backend/restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+port=13002
+# 根据端口号查询对应的pid
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+
+# 杀掉对应的进程，如果pid不存在，则不执行
+if [  -n  "$pid"  ];  then
+    kill  -9  $pid;
+fi
+
+go build
+nohup ./backend 2>&1 &
