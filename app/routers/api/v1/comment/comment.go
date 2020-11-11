5375a8638070ed8fe3419f41234a18510687398a
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-11 16:03:59 +0800

    fix

--- app/routers/api/v1/comment/comment.go
+++ app/routers/api/v1/comment/comment.go
@@ -101,19 +101,20 @@ func PublishReply(c *gin.Context) {
 // @Param   size	  	  query  	string 	true  "每页展示多少 最多50条"
 // @Param   video_id	  query  	string 	true  "视频id"
 // @Param   sort_type     query     string  true  "排序规则 0 时间 1 热度"
+// @Param   user_id	    query  	string 	true  "用户id"
 // @Success 200 {array}  mcomment.VideoComments
 // @Failure 500 {string} json "{"code":500,"data":{},"msg":"fail","tm":"1588888888"}"
 // @Router /api/v1/comment/list [get]
 // 评论列表
 func CommentList(c *gin.Context) {
 	reply := errdef.New(c)
-	userId, _ := c.Get(consts.USER_ID)
+	userId := c.Query("user_id")
 	videoId := c.Query("video_id")
 	sortType := c.DefaultQuery("sort_type", "0")
 	page, size := util.PageInfo(c.Query("page"), c.Query("size"))
 
 	svc := comment.New(c)
-	list := svc.GetVideoComments(userId.(string), videoId, sortType, page, size)
+	list := svc.GetVideoComments(userId, videoId, sortType, page, size)
 
 	reply.Data["list"] = list
 	reply.Response(http.StatusOK, errdef.SUCCESS)
@@ -133,6 +134,7 @@ func CommentList(c *gin.Context) {
 // @Param   page	  	  query  	string 	true  "页码 从1开始"
 // @Param   size	  	  query  	string 	true  "每页展示多少 最多50条"
 // @Param   video_id	  query  	string 	true  "视频id"
+// @Param   user_id	    query  	string 	true  "用户id"
 // @Param   comment_id	  query  	string 	true  "评论id"
 // @Success 200 {array}  mcomment.ReplyComment
 // @Failure 500 {string} json "{"code":500,"data":{},"msg":"fail","tm":"1588888888"}"
@@ -140,14 +142,14 @@ func CommentList(c *gin.Context) {
 // 回复列表
 func ReplyList(c *gin.Context) {
 	reply := errdef.New(c)
-	userId, _ := c.Get(consts.USER_ID)
+	userId := c.Query("user_id")
 	commentId := c.Query("comment_id")
 	videoId := c.Query("video_id")
 	page, size := util.PageInfo(c.Query("page"), c.Query("size"))
 
 	svc := comment.New(c)
 	// 获取评论回复列表
-	syscode, list := svc.GetCommentReplyList(userId.(string), videoId, commentId, page, size)
+	syscode, list := svc.GetCommentReplyList(userId, videoId, commentId, page, size)
 	reply.Data["list"] = list
 	reply.Response(http.StatusOK, syscode)
 }
