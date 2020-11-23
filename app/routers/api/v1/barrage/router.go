c480206a3ac03c7eacfb112dfb32b15bc284322b
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-23 15:20:18 +0800

    fix

--- app/routers/api/v1/barrage/router.go
+++ app/routers/api/v1/barrage/router.go
@@ -1,17 +1,17 @@
 package barrage
 
 import (
-	"github.com/gin-gonic/gin"
-	"sports_service/server/middleware/token"
+  "github.com/gin-gonic/gin"
+  "sports_service/server/middleware/token"
 )
 
 // 弹幕模块路由
 func Router(engine *gin.Engine) {
 	api := engine.Group("/api/v1")
-	barrage := api.Group("/barrage", token.TokenAuth())
+	barrage := api.Group("/barrage")
 	{
 		// 发送弹幕
-		barrage.POST("/send", SendBarrage)
+		barrage.POST("/send", token.TokenAuth(), SendBarrage)
 		// 获取视频弹幕内容（按时长区间获取）
 		barrage.GET("/video/list", VideoBarrage)
 	}
