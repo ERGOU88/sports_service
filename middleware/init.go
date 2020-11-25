147c03504dbee8cd9d73890d2fd11cc698599418
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-25 17:25:13 +0800

    fix

--- middleware/init.go
+++ middleware/init.go
@@ -4,6 +4,7 @@ import (
   "github.com/gin-gonic/gin"
   "sports_service/server/log"
   "sports_service/server/middleware/engineLog"
+  "sports_service/server/middleware/header"
 )
 
 // 初始化部分中间件
@@ -11,5 +12,5 @@ func InitMiddleware(engine *gin.Engine, log log.ILogger, showColor bool) {
   // 日志中间件
   engine.Use(engineLog.EngineLog(log, showColor))
 	// 跨域处理
-	//engine.Use(header.Options)
+	engine.Use(header.Options)
 }
