9a6c7b9b6ab2c612157f926b3bcdf49853533b24
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-25 15:04:33 +0800

    fix

--- middleware/header/header.go
+++ middleware/header/header.go
@@ -17,14 +17,25 @@ func Options(c *gin.Context) {
     //c.Header("Content-Type", "application/json")
     //c.AbortWithStatus(200)
   //}
-	c.Header("Access-Control-Allow-Origin", "*")
+
+  //if c.Request.Method != "OPTIONS" {
+  //  c.Next()
+  //} else {
+  //  c.Header("Access-Control-Allow-Origin", "http://fpv-web-qa.youzu.com")
+  //  c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+  //  c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept, app-id, signature, sq-id, timestamp")
+  //  c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
+  //  c.Header("Access-Control-Allow-Headers", "Cookie")
+  //  //c.Header("Content-Type", "application/json")
+  //  c.AbortWithStatus(200)
+  //}
+	c.Header("Access-Control-Allow-Origin", "http://fpv-web-qa.youzu.com")
   c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 	c.Header("Access-Control-Allow-Headers", "x-requested-with")
 	c.Header("Access-Control-Allow-Headers", "Cookie")
 	c.Header("Access-Control-Allow-Headers", "Authorization")
 	c.Header("Access-Control-Allow-Headers", "auth")
 	c.Header("Access-Control-Allow-Headers", "Content-Type")
-  c.Header("Content-Type", "application/json")
 	// 允许请求带有验证信息
 	c.Header("Access-Control-Allow-Credentials", "true")
 }
