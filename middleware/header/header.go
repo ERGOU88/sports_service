c09aa8dba6347f0613091fed3b97ea80d57d44a0
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-25 17:30:50 +0800

    fix

--- middleware/header/header.go
+++ middleware/header/header.go
@@ -9,15 +9,15 @@ import (
 // 跨域处理
 func Options(c *gin.Context) {
   if c.Request.Method == "OPTIONS" {
-    //c.Header("Access-Control-Allow-Origin", "*")
-    //c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
-    //c.Header("Access-Control-Allow-Headers", "x-requested-with")
-    //c.Header("Access-Control-Allow-Headers", "Cookie")
-    //c.Header("Access-Control-Allow-Headers", "Authorization")
-    //c.Header("Access-Control-Allow-Headers", "auth")
-    //c.Header("Access-Control-Allow-Headers", "Content-Type")
-    //// 允许请求带有验证信息
-    //c.Header("Access-Control-Allow-Credentials", "true")
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+    c.Header("Access-Control-Allow-Headers", "x-requested-with")
+    c.Header("Access-Control-Allow-Headers", "Cookie")
+    c.Header("Access-Control-Allow-Headers", "Authorization")
+    c.Header("Access-Control-Allow-Headers", "auth")
+    c.Header("Access-Control-Allow-Headers", "Content-Type")
+    // 允许请求带有验证信息
+    c.Header("Access-Control-Allow-Credentials", "true")
     c.AbortWithStatus(200)
   } else {
     c.Header("Access-Control-Allow-Origin", "*")
