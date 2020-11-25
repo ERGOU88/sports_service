23700c2beb8b105474b6f66fe6282bf06526cc71
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-11-25 17:06:28 +0800

    fix

--- app/routers/routers.go
+++ app/routers/routers.go
@@ -24,6 +24,7 @@ import (
 // 路由初始化
 func InitRouters(engine *gin.Engine) {
   engine.Use(func(c *gin.Context) {
+    if c.Request.Method != "OPTIONS" {
       c.Header("Access-Control-Allow-Origin", "*")
       c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
       c.Header("Access-Control-Allow-Headers", "x-requested-with")
@@ -33,6 +34,8 @@ func InitRouters(engine *gin.Engine) {
       c.Header("Access-Control-Allow-Headers", "Content-Type")
       // 允许请求带有验证信息
       c.Header("Access-Control-Allow-Credentials", "true")
+      c.AbortWithStatus(200)
+    }
   })
 	// 初始化中间件
 	middleware.InitMiddleware(engine, log.Log, config.Global.Log.ShowColor)
