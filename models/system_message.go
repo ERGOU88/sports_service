4ee321057ac0982eb1a01ec5fe6d5e8ed19c4e3a
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-12-22 18:53:41 +0800

    fix struct

--- models/system_message.go
+++ models/system_message.go
@@ -10,7 +10,7 @@ type SystemMessage struct {
 	SendTime      int    `json:"send_time" xorm:"not null comment('发送时间') INT(11)"`
 	SendType      int    `json:"send_type" xorm:"not null default 0 comment('0.默认为系统通知 1 活动通知  2 待支付订单延时提示消息（15分钟 用户端） 3. 待咨询订单通知(开始前1天 用户端及咨询师端) 4.待咨询订单通知(开始前1小时 用户端及咨询师端) 5. 咨询师写评估报告通知（结束后1小时 咨询师端）6. 咨询师写评估报告通知（结束后24小时 咨询师端）') index TINYINT(1)"`
 	Status        int    `json:"status" xorm:"not null default 0 comment('0 未读 1 已读  默认未读') TINYINT(1)"`
-	SystemContent string `json:"system_content" xorm:"not null comment('通知内容') TEXT"`
+	SystemContent string `json:"system_content" xorm:"not null comment('通知内容') MEDIUMTEXT"`
 	SystemId      int64  `json:"system_id" xorm:"not null pk autoincr comment('系统通知ID') BIGINT(20)"`
 	SystemTopic   string `json:"system_topic" xorm:"not null comment('通知标题') MEDIUMTEXT"`
 }
