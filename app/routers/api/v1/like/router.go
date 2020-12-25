a60c5db10456880095969e428c5df0e76acc8b30
陈涛(陈二狗) <tao_chen@yoozoo.com>
2020-12-25 16:02:51 +0800

    fix

--- app/routers/api/v1/like/router.go
+++ app/routers/api/v1/like/router.go
@@ -1,26 +1,26 @@
 package like
 
 import (
-	"github.com/gin-gonic/gin"
-	"sports_service/server/middleware/token"
+  "github.com/gin-gonic/gin"
+  "sports_service/server/middleware/token"
 )
 
 // 点赞模块路由
 func Router(engine *gin.Engine) {
 	api := engine.Group("/api/v1")
-	like := api.Group("/like", token.TokenAuth())
+	like := api.Group("/like")
 	{
 		// 视频点赞
-		like.POST("/video", GiveLikeForVideo)
+		like.POST("/video", token.TokenAuth(), GiveLikeForVideo)
 		// 视频取消点赞
-		like.POST("/video/cancel", CancelLikeForVideo)
+		like.POST("/video/cancel", token.TokenAuth(), CancelLikeForVideo)
 		// 用户点赞的视频列表
 		like.GET("/video/list", LikeVideoList)
 		// 查看其他用户点赞的视频列表
 		like.GET("/other/list", OtherUserLikeVideoList)
 		// 评论点赞
-		like.POST("/comment", GiveLikeForComment)
+		like.POST("/comment", token.TokenAuth(), GiveLikeForComment)
 		// 评论取消点赞
-		like.POST("/comment/cancel", CancelLikeForComment)
+		like.POST("/comment/cancel", token.TokenAuth(), CancelLikeForComment)
 	}
 }
