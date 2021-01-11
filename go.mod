87d3805e3b5b1fc73ba5ca9a8cbe464f641626a7
陈涛(陈二狗) <tao_chen@yoozoo.com>
2021-01-11 12:33:36 +0800

    fix

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/antlinker/go-cmap v0.0.0-20160407022646-0c5e57012e96 // indirect
 	github.com/antlinker/go-dirtyfilter v1.2.0
 	github.com/elazarl/goproxy v0.0.0-20200809112317-0581fc3aee2d // indirect
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/garyburd/redigo v1.6.2
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
@@ -18,7 +19,11 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.4.2
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/json-iterator/go v1.1.10
+	github.com/lestrrat/go-envload v0.0.0-20180220120943-6ed08b54a570 // indirect
+	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
+	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
 	github.com/nsqio/go-nsq v1.0.8
 	github.com/parnurzeal/gorequest v0.2.16
 	github.com/rs/xid v1.2.1
@@ -26,6 +31,7 @@ require (
 	github.com/streadway/amqp v1.0.0
 	github.com/swaggo/gin-swagger v1.2.0
 	github.com/swaggo/swag v1.5.1
+	github.com/tebeka/strftime v0.1.5 // indirect
 	github.com/tencentcloud/tencentcloud-sdk-go v1.0.30
 	github.com/tencentyun/cos-go-sdk-v5 v0.7.11 // indirect
 	github.com/tencentyun/qcloud-cos-sts-sdk v0.0.0-20201001014220-f25178b7257d
--- log/zap/zap.go
+++ log/zap/zap.go
@@ -1,83 +1,111 @@
 package zap
 
 import (
-	"bytes"
-	"sports_service/server/log/zap/zlog"
-	"fmt"
-	"runtime"
-	"strconv"
-	"time"
-
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+  "bytes"
+  "fmt"
+  "github.com/lestrrat/go-file-rotatelogs"
+  "go.uber.org/zap"
+  "go.uber.org/zap/zapcore"
+  "io"
+  "runtime"
+  "sports_service/server/log/zap/zlog"
+  "strconv"
+  "time"
 )
 
 func getGID() uint64 {
-	b := make([]byte, 64)
-	b = b[:runtime.Stack(b, false)]
-	b = bytes.TrimPrefix(b, []byte("goroutine "))
-	b = b[:bytes.IndexByte(b, ' ')]
-	n, _ := strconv.ParseUint(string(b), 10, 64)
-	return n
+  b := make([]byte, 64)
+  b = b[:runtime.Stack(b, false)]
+  b = bytes.TrimPrefix(b, []byte("goroutine "))
+  b = b[:bytes.IndexByte(b, ' ')]
+  n, _ := strconv.ParseUint(string(b), 10, 64)
+  return n
 }
 
 // InitZapLog 初始化日志
 func InitZapLog(logPath string, showColor bool, level int) *zlog.SimpleZapLogger {
-	var encoding string
-	var encodetime zapcore.TimeEncoder
-	if showColor {
-		encoding = "console"
-		encodetime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-			enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]-" + strconv.FormatUint(getGID(), 10))
-		}
-	} else {
-		encoding = "json"
-		encodetime = zapcore.ISO8601TimeEncoder
-	}
-	encoderCfg := zapcore.EncoderConfig{
-		// Keys can be anything except the empty string.
-		TimeKey:        "time",
-		LevelKey:       "level",
-		NameKey:        "app_id",
-		CallerKey:      "caller",
-		MessageKey:     "msg",
-		StacktraceKey:  "S",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.CapitalLevelEncoder,
-		EncodeTime:     encodetime,
-		EncodeDuration: zapcore.StringDurationEncoder,
-		EncodeCaller:   zapcore.FullCallerEncoder,
-	}
+  var encoding string
+  var encodetime zapcore.TimeEncoder
+  if showColor {
+    encoding = "console"
+    encodetime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+      enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]-" + strconv.FormatUint(getGID(), 10))
+    }
+  } else {
+    encoding = "json"
+    encodetime = zapcore.ISO8601TimeEncoder
+  }
+  encoderCfg := zapcore.EncoderConfig{
+    // Keys can be anything except the empty string.
+    TimeKey:        "time",
+    LevelKey:       "level",
+    NameKey:        "app_id",
+    CallerKey:      "file",
+    MessageKey:     "msg",
+    StacktraceKey:  "S",
+    LineEnding:     zapcore.DefaultLineEnding,
+    EncodeLevel:    zapcore.CapitalLevelEncoder,
+    EncodeTime:     encodetime,
+    EncodeDuration: zapcore.StringDurationEncoder,
+    EncodeCaller:   zapcore.FullCallerEncoder,
+  }
 
-	var lev zapcore.Level
-	switch level {
-	case 0:
-		lev = zap.DebugLevel
-	case 1:
-		lev = zap.InfoLevel
-	case 2:
-		lev = zap.WarnLevel
-	case 3:
-		lev = zap.ErrorLevel
-	}
-	currLevel := zap.NewAtomicLevelAt(lev)
+  var lev zapcore.Level
+  switch level {
+  case 0:
+    lev = zap.DebugLevel
+  case 1:
+    lev = zap.InfoLevel
+  case 2:
+    lev = zap.WarnLevel
+  case 3:
+    lev = zap.ErrorLevel
+  }
+  //currLevel := zap.NewAtomicLevelAt(lev)
+  //
+  //customCfg := zap.Config{
+  //  Level:             currLevel,
+  //  Development:       true,
+  //  DisableCaller:     false,
+  //  DisableStacktrace: false,
+  //  Encoding:          encoding,
+  //  EncoderConfig:     encoderCfg,
+  //  OutputPaths:       []string{fmt.Sprintf("%s%s", logPath, ".%Y%m%d%H%M%S")},
+  //  ErrorOutputPaths:  []string{"stderr"},
+  //}
 
-	customCfg := zap.Config{
-		Level:             currLevel,
-		Development:       true,
-		DisableCaller:     false,
-		DisableStacktrace: false,
-		Encoding:          encoding,
-		EncoderConfig:     encoderCfg,
-		OutputPaths:       []string{logPath},
-		ErrorOutputPaths:  []string{"stderr"},
-	}
+  var core zapcore.Core
+  ioWriter := getWriter(logPath)
+  if encoding == "json" {
+    core = zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), zapcore.AddSync(ioWriter), lev)
+  } else {
+    core = zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg), zapcore.AddSync(ioWriter), lev)
+  }
 
-	logger, err := customCfg.Build(zap.AddCallerSkip(1), zap.AddStacktrace(zap.WarnLevel))
-	if err != nil {
-		panic(fmt.Errorf("init log error %v", err))
-	}
-	l := logger.Named("fpv")
-	defer l.Sync()
-	return zlog.NewSimpleZapLogger(l.Sugar())
+  logger := zap.New(core, zap.AddCallerSkip(1), zap.AddStacktrace(zap.WarnLevel))
+  //logger, err := customCfg.Build(zap.AddCallerSkip(1), zap.AddStacktrace(zap.WarnLevel))
+  //if err != nil {
+  //  panic(fmt.Errorf("init log error %v", err))
+  //}
+
+  l := logger.Named("fpv")
+  defer l.Sync()
+  return zlog.NewSimpleZapLogger(l.Sugar())
+}
+
+// 日志文件切割
+func getWriter(filename string) io.Writer {
+  // 保存30天内的日志，每24小时(整点)分割一次日志
+  hook, err := rotatelogs.New(
+    fmt.Sprintf(filename, "%Y%m%d"),
+    //rotatelogs.WithLinkName(filename),
+    rotatelogs.WithMaxAge(time.Hour * 24 * 90),
+    rotatelogs.WithRotationTime(time.Hour * 24),
+  )
+
+  if err != nil {
+    panic(err)
+  }
+
+  return hook
 }
