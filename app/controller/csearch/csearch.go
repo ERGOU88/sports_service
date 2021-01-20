cc4fdeb3b1ebb957586a28c31e7f0147c99bb15b
陈涛(陈二狗) <tao_chen@yoozoo.com>
2021-01-20 15:44:42 +0800

    fix

--- app/controller/csearch/csearch.go
+++ app/controller/csearch/csearch.go
@@ -81,7 +81,7 @@ func (svc *SearchModule) ColligateSearch(userId, name string) ([]*mvideo.VideoDe
 
 // 推荐视频 默认取两条
 func (svc *SearchModule) RecommendVideo() []*mvideo.VideoDetailInfo {
-  offset := util.GenerateRandnum(0, 10)
+  offset := util.GenerateRandnum(0, int(svc.video.GetVideoTotalCount())-2)
   videos := svc.video.GetRecommendVideos(int32(offset), 2)
   if videos == nil {
     log.Log.Error("search_trace: get recommend video fail")
