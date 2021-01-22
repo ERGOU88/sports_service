6e535ad123a80c6e18abdb78d7e6ad0a8f195563
陈涛(陈二狗) <tao_chen@yoozoo.com>
2021-01-22 12:12:39 +0800

    fix

--- global/consts/push_consts.go
+++ global/consts/push_consts.go
@@ -25,6 +25,18 @@ const (
 
 )
 
+var NotifyDoc = map[MessageType]string{
+  MSG_TYPE_SYSTEM_NOTIFY :               "系统类推送",
+  MSG_TYPE_ACTIVITY_NOTIFY:              "活动类推送消息",
+  MSG_TYPE_VIDEO_LIKE_NOTIFY :           "视频点赞推送",
+  MSG_TYPE_COMMENT_LIKE_NOTIFY :         "评论/回复点赞推送",
+  MSG_TYPE_VIDEO_COLLECT_NOTIFY :        "收藏视频推送",
+  MSG_TYPE_FOCUS_NOTIFY:                 "关注推送",
+  MSG_TYPE_FOCUS_USER_PUBLISH_NOTIFY:    "关注的用户发布新视频推送",
+  MSG_TYPE_VIDEO_COMMENT_NOTIFY:         "视频评论推送",
+  MSG_TYPE_VIDEO_REPLY_NOTIFY:           "视频回复推送",
+}
+
 const (
   // android: notification 通知栏推送  message 自定义推送
   ANDROID_PUSH_TYPE_NOTIFICATION  = "notification"
