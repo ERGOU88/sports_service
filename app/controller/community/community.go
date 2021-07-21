d4658421a6cc90952ac1774a2383d2bd6044f18a
陈涛 <chentao@bluetrans.local>
2021-07-21 19:53:56 +0800

    fix

--- app/controller/community/community.go
+++ app/controller/community/community.go
@@ -168,10 +168,12 @@ func (svc *CommunityModule) GetPostListBySection(page, size int, userId, section
 
 
 	for _, item := range list {
-		item.Topics, err = svc.post.GetPostTopic(fmt.Sprint(item.Id))
-		if item.Topics == nil || err != nil  {
+		topics, err := svc.post.GetPostTopic(fmt.Sprint(item.Id))
+		if topics == nil || err != nil  {
 			log.Log.Errorf("community_trace: get post topic fail, err:%s, item.Topics:%v", err, item.Topics)
 			item.Topics = []*models.PostingTopic{}
+		} else {
+			item.Topics = topics
 		}
 
 
