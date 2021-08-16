74696378eacca9ccb6cbf568d84c54560b57b57d
陈涛 <chentao@bluetrans.local>
2021-08-16 15:18:57 +0800

    fix

--- app/controller/cappointment/base.go
+++ app/controller/cappointment/base.go
@@ -42,6 +42,7 @@ func (svc *base) AppointmentDateInfo(days, appointmentType int) interface{} {
 	list := svc.GetAppointmentDate(days)
 	res := make([]*mappointment.WeekInfo, len(list))
 	id := 0
+	week := -1
 	for index, v := range list {
 		info := &mappointment.WeekInfo{
 			Id: v.Id,
@@ -71,12 +72,17 @@ func (svc *base) AppointmentDateInfo(days, appointmentType int) interface{} {
 			id = v.Id
 		}
 
+		if week == -1 && info.Total > 0 {
+			week = v.Week
+		}
+
 		res[index] = info
 	}
 
 	dateInfo := &mappointment.DateInfo{
 		List: res,
 		Id: id,
+		Week: week,
 	}
 
 	return dateInfo
