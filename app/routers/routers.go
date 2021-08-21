b3759241beb89d0eaa96f7d1a24df8925eb8da5e
陈涛 <chentao@bluetrans.local>
2021-08-21 15:21:26 +0800

    fix

--- app/routers/routers.go
+++ app/routers/routers.go
@@ -1,7 +1,6 @@
 package routers
 
 import (
-	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"sports_service/server/app/config"
@@ -44,11 +43,11 @@ func InitRouters(engine *gin.Engine) {
 	}
 
 	engine.Use(gin.Recovery())
+	//engine.Use(gzip.Gzip(gzip.DefaultCompression))
 	engine.Any("/", func(c *gin.Context) {
 		c.Status(http.StatusOK)
 		return
 	})
-	engine.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	// 初始化接口
 	client.Router(engine)
