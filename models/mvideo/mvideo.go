e57327af48e4a5bc6a3f6b26c1fe90943814154d
陈涛 <chentao@bluetrans.local>
2021-09-10 11:47:12 +0800

    fix

--- models/mvideo/mvideo.go
+++ models/mvideo/mvideo.go
@@ -126,6 +126,7 @@ type VideoDetailInfo struct {
 	Describe      string                `json:"describe"  example:"描述"`              // 描述
 	Cover         string                `json:"cover"  example:"封面"`                 // 封面
 	VideoAddr     string                `json:"video_addr"  example:"视频地址"`         // 视频地址
+	Size          int64                 `json:"size"`                                 // 视频大小 字节数
 	IsRecommend   int                   `json:"is_recommend" example:"0"`             // 是否推荐
 	IsTop         int                   `json:"is_top"  example:"0"`                 // 是否置顶
 	VideoDuration int                   `json:"video_duration" example:"100000"`       // 视频时长
--- tools/live/live_test.go
+++ tools/live/live_test.go
@@ -7,20 +7,20 @@ import (
 
 // 获取推流地址
 func TestGenPushStream(t *testing.T) {
-	txTime := BuildTxTime(7200)
-	txSecret := BuildTxSecret(LIVE_PUSH_KEY,"654321", 7200*40)
+	txTime := BuildTxTime(7200 * 40)
+	txSecret := BuildTxSecret(LIVE_PUSH_KEY,"123456", 7200 * 40)
 	stream := fmt.Sprintf("rtmp://%s/live/%s?txSecret=%s&txTime=%s", PUSH_STREAM_HOST,
-		"123", txSecret, txTime)
+		"123456", txSecret, txTime)
 	t.Logf("stream:%s", stream)
 }
 
 func TestGenPullStream(t *testing.T) {
-	txTime := BuildTxTime(7200)
-	txSecret := BuildTxSecret(LIVE_PULL_KEY, "654321", 7200*40)
+	txTime := BuildTxTime(7200 * 40)
+	txSecret := BuildTxSecret(LIVE_PULL_KEY, "123456", 7200 * 40)
 	streamInfo := &PullStreamInfo{
-		RtmpAddr: fmt.Sprintf("rtmp://%s/live/%s?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123", txSecret, txTime),
-		FlvAddr: fmt.Sprintf("https://%s/live/%s.flv?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123", txSecret, txTime),
-		HlsAddr: fmt.Sprintf("https://%s/live/%s.m3u8?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123", txSecret, txTime),
+		RtmpAddr: fmt.Sprintf("rtmp://%s/live/%s?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123456", txSecret, txTime),
+		FlvAddr: fmt.Sprintf("https://%s/live/%s.flv?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123456", txSecret, txTime),
+		HlsAddr: fmt.Sprintf("https://%s/live/%s.m3u8?txSecret=%s&txTime=%s", PULL_STREAM_HOST, "123456", txSecret, txTime),
 	}
 
 	t.Logf("streamInfo:%+v", streamInfo)
