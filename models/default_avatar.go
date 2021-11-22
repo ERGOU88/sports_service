ae7a30752055a3f7a474cef0f8900914c9caacc3
陈涛 <chentao@bluetrans.local>
2021-11-22 13:43:55 +0800

    fix

--- models/default_avatar.go
+++ models/default_avatar.go
@@ -1,8 +1,10 @@
 package models
 
+import "sports_service/server/tools/tencentCloud"
+
 type DefaultAvatar struct {
 	Id        int64  `json:"id" xorm:"pk autoincr comment('主键id') BIGINT(20)"`
-	Avatar    string `json:"avatar" xorm:"not null comment('头像地址') VARCHAR(128)"`
+	Avatar    tencentCloud.BucketURI `json:"avatar" xorm:"not null comment('头像地址') VARCHAR(128)"`
 	Sortorder int    `json:"sortorder" xorm:"not null default 0 comment('排序权重') INT(11)"`
 	CreateAt  int    `json:"create_at" xorm:"not null default 0 comment('创建时间') INT(11)"`
 	UpdateAt  int    `json:"update_at" xorm:"not null default 0 comment('更新时间') INT(11)"`
