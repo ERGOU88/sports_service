30b7ce6873e16020c9e6c60c3635a795d4522609
陈涛 <chentao@bluetrans.local>
2021-11-22 14:25:39 +0800

    fix

--- global/consts/venue_consts.go
+++ global/consts/venue_consts.go
@@ -1,5 +1,5 @@
 package consts
 
 const (
-	VENUE_VIP_IMAGE = "https://fpv-1253904687.cos.ap-shanghai.myqcloud.com/xfly_vip.png"
+	VENUE_VIP_IMAGE = "https://fpv-1253904687.cos.ap-shanghai.myqcloud.com/fpv/xfly_vip.png"
 )
