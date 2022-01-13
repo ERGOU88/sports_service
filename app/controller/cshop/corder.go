05a3d2174ea64d9ac948b2b7d185e9820bb629c8
陈涛 <chentao@bluetrans.local>
2022-01-13 18:54:14 +0800

    fix

--- app/controller/cshop/corder.go
+++ app/controller/cshop/corder.go
@@ -612,8 +612,8 @@ func (svc *ShopModule) DeleteOrder(param *mshop.ChangeOrderReq) int {
 		return errdef.SHOP_ORDER_DELETE_FAIL
 	}
 	
-	// 支付状态 != 已支付 || 配送状态 != 已签收 && 支付状态 != 未支付/支付超时
-	if (order.PayStatus != consts.SHOP_ORDER_TYPE_PAID || order.DeliveryStatus != consts.HAS_SIGNED) && order.PayStatus != consts.SHOP_ORDER_TYPE_UNPAID {
+	// 已支付 且 未完成的订单 不能删除
+	if order.PayStatus == consts.SHOP_ORDER_TYPE_PAID && order.DeliveryStatus != consts.HAS_SIGNED {
 		return errdef.SHOP_ORDER_DELETE_FAIL
 	}
 	
