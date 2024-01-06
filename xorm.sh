dfe8cf2a50ff75a9969077d1523987e9d2d7b6a5
ergou <123@gmail.com>
2024-01-06 17:35:39 +0800

    fix xorm.sh

--- xorm.sh
+++ xorm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # 数据表->数据结构
 # tips: ./xorm reverse mysql $username:$password@tcp\($host\)/$dbname?charset=utf8 配置文件目录 输出目录
-cd ../pkg/mod/github.com/go-xorm/cmd/xorm@v0.0.0-20190426080617-f87981e709a1
-xorm reverse mysql root:bluetrans888@tcp\(10.1.3.12:3306\)/sports_service?charset=utf8 ../../../../../../server/models/modelTpl ../../../../../../server/models/
-xorm reverse mysql root:bluetrans888@tcp\(10.1.3.12:3306\)/venue?charset=utf8 ../../../../../../server/models/modelTpl ../../../../../../server/models/
+xorm reverse mysql root:123456@tcp\(127.0.0.1:3306\)/sports_service?charset=utf8 models/modelTpl models/
+xorm reverse mysql root:123456@tcp\(127.0.0.1:3306\)/venue?charset=utf8 models/modelTpl models/
